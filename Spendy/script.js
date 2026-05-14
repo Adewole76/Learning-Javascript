ea0f4e299942ed4199c67236e302beadea1e9c4b
YeezyTheDev <adewoleoluwasegun7@gmail.com>
2026-05-14 16:53:11 +0100

    expense id

--- Spendy/script.js
+++ Spendy/script.js
@@ -1,9 +1,17 @@
 const transactionContainer = document.querySelector('.transaction-container');
 const transactionsArray = []
-const addTransaction =(arr, transcationid, transactiontype, transactionamount, transactiondescription, transactioncategory, transactioncreatedAt) => {
+const addTransaction =(transactiontype, transactionamount, transactiondescription, transactioncategory, transactionDate) => {
     let transactionObject = {
-        transactionid: id,
-        transactionType:type, 
-        transactio
+        id: 'txn_' + Date.now() + Math.floor(Math.random() * 1000),
+        type: transactiontype,
+        amount: transactionamount,
+        description: transactiondescription,
+        category: transactioncategory,
+        date: transactionDate
+    }
+    if(Number.isFinite(transactionamount)){
+       transactionsArray.push(transactionObject); 
+    }else{
+
     }
 }
