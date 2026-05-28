1506231768963e1577e5f36bbcd1894e7308c2a9
YeezyTheDev <adewoleoluwasegun7@gmail.com>
2026-05-28 22:11:38 +0100

    fixing expense category problem

--- Spendy/script.js
+++ Spendy/script.js
@@ -1,22 +1,25 @@
 'use strict'
 const transactionContainer = document.querySelector('.transaction-container');
 const addTransactionButton = document.querySelector('.add-transaction');
-const totalAmountheader = document.querySelector('.total-balanc');
-console.log(totalAmountheader);
-const updatetotalAmountBtn = document.querySelector('.update-amount-button')
+const totalAmount = document.querySelector('.total-balances');
+console.log(totalAmount);
+const updatetotalAmountBtn = document.querySelector('.update-amount-button');
 const transactionForm = document.querySelector('.transaction-form');
 const backDropOverlay = document.querySelector('.overlay');
 const incomeButton = document.querySelector('.income-button');
-const ExpenseButton = document.querySelector('.Expense-button')
+const ExpenseButton = document.querySelector('.Expense-button');
 const ExpenseCategory = document.querySelector('.Expense');
 const IncomeCategory = document.querySelector('.Income');
 const actualAddtransaction = document.querySelector('.add')
 const closeButton = document.querySelector('.close-button');
+const selectExpense = document.querySelector('.ExpenseCat')
+const selectIncome = document.querySelector('.incomeCat');
 
 //form input DOM variables
-const amountInput =document.querySelector('.amount-input');
+const amountInput = document.querySelector('.amount-input');
 const description_input = document.querySelector('.description-input');
-const CategoryInput = document.getElementById('category-select');
+const CategoryInput = document.querySelector('.category-select');
+console.log(CategoryInput);
 
 //loading tasks array from localStorage to ensure info persistence
 let storedTasks = localStorage.getItem('transactions');
@@ -24,10 +27,12 @@ let parsedStoredTasks =JSON.parse(storedTasks);
 let transactionsArray = parsedStoredTasks ? parsedStoredTasks:[];
 console.log(transactionsArray);
 
+//user monetary info
 let totalBalance = 0;
 let totalIncome = 0;
 let totalExpense =0;
 
+//Variable tracking transactions category
 let transactionType = 'Expense'
 //Functions to add a transaction
 const addTransaction =(transactiontype, transactionamount, transactiondescription, transactioncategory) => {
@@ -61,13 +66,15 @@ incomeButton.addEventListener('click', function(){
     ExpenseCategory.classList.add('hidden');
     transactionType ='Income';
     console.log(transactionType);
+    console.log(CategoryInput)
 })
 
 ExpenseButton.addEventListener('click', function(){
- ExpenseCategory.classList.remove('hidden')
+ ExpenseCategory.classList.remove('hidden');
   IncomeCategory.classList.add('hidden');
   transactionType ='Expense';
   console.log(transactionType)
+  console.log(CategoryInput);
 })
 
 actualAddtransaction.addEventListener('click', function(){
@@ -98,12 +105,16 @@ const mappingTransaction = () => {
 mappingTransaction();
 const deleteButtons = document.querySelectorAll(".delete-button");
 console.log(deleteButtons);
+
+//delete Transaction from transactions Array
 const deleteTransaction = (arr) => {
     let filteredTransactionsArray = arr.filter(transaction => transaction.type !== 'Deleted');
     transactionsArray = filteredTransactionsArray;
     localStorage.setItem('transactions', JSON.stringify(transactionsArray))
- }
+deleteTransaction(arr); 
+}
  
+//event listener for all delete buttons 
 for(let i = 0; i < deleteButtons.length; i++){
     deleteButtons[i].addEventListener('click', function(){
         console.log(transactionsArray[i]);
@@ -111,11 +122,18 @@ for(let i = 0; i < deleteButtons.length; i++){
         deleteTransaction(transactionsArray); 
     })
 }
+//for loop for all transaction amounts 
 for(let i = 0; i < transactionsArray.length; i++){
     let transactionAmount = Number(transactionsArray[i].amount);
+    if(transactionsArray[i].type === 'Income'){
     totalBalance = totalBalance + transactionAmount;
     console.log(totalBalance);
+    }else if(transactionsArray[i].type === 'Expense'){
+    totalBalance = totalBalance - transactionAmount;
+    console.log(totalBalance);
+    }
 }
-totalAmountheader.innerHTML = totalBalance;
+totalAmount.innerHTML = totalBalance;
+console.log(totalAmount.innerHTML);
 
 
