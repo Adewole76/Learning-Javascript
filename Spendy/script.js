f625cb3d48a2df1860f7f9b0c17c64a68bad9817
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-09-02 22:28:00 +0100

    re ordered download button eventlistener to prevent naming conflict between two pages

--- Spendy/script.js
+++ Spendy/script.js
@@ -312,43 +312,37 @@ transactionContainer.addEventListener('click', function(e) {
 
 
 
-function downloadCSV(jsonData, filename = "export.csv") {
-  const headers = Object.keys(jsonData[0]).join(",");
-  
-  const rows = jsonData.map(row => 
-    Object.values(row)
-      .map(value => {
-        let strValue = String(value);
-        if (strValue.includes(",") || strValue.includes('"')) {
-          strValue = `"${strValue.replace(/"/g, '""')}"`;
-        }
-        return strValue;
-      })
-      .join(",")
-  );
-
-  const csvContent = [headers, ...rows].join("\n");
-
-  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-  const url = URL.createObjectURL(blob);
-
-  // 4. Trigger programmatic download
-  const link = document.createElement("a");
-  link.href = url;
-  link.setAttribute("download", filename);
-  link.style.visibility = "hidden";
-  
-  document.body.appendChild(link);
-  link.click();
-  
-  // 5. Clean up the DOM and memory
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
-}
-console.log(downloadBtn)
-downloadBtn.addEventListener('click', function(){
-    downloadCSV(transactionsArray, "users_list.csv");
-})
+ function downloadCSV(jsonData, filename = "export.csv") {
+   const headers = Object.keys(jsonData[0]).join(",");
+   const rows = jsonData.map(row => 
+     Object.values(row)
+       .map(value => {
+         let strValue = String(value);
+         if (strValue.includes(",") || strValue.includes('"')) {
+           strValue = `"${strValue.replace(/"/g, '""')}"`;
+         }
+         return strValue;
+       })
+       .join(",")
+   )
+   const csvContent = [headers, ...rows].join("\n")
+   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+   const url = URL.createObjectURL(blob)
+   // 4. Trigger programmatic download
+   const link = document.createElement("a");
+   link.href = url;
+   link.setAttribute("download", filename);
+   link.style.visibility = "hidden";
+   document.body.appendChild(link);
+   link.click();
+   // 5. Clean up the DOM and memory
+   document.body.removeChild(link);
+   URL.revokeObjectURL(url);
+ }
+ console.log(downloadBtn)
+ downloadBtn.addEventListener('click', function(){
+     downloadCSV(transactionsArray, "users_list.csv");
+ })
 
 
 
@@ -356,7 +350,7 @@ console.log(totalBalance);
 totalAmount.innerHTML = totalBalance?totalBalance:0;
 incomeContainer.innerHTML = totalIncome?totalIncome:0;
 expenseContainer.innerHTML = totalExpense?totalExpense:0;
+console.log(totalBalance)
 console.log(totalAmount.innerHTML);
-const headers = ['Date','Description', 'Category', 'Amount'];
-const headerRow = headers.join(',') + '\n';
+
 
